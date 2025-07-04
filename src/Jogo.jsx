2b00f414c2c8cb443845fc060eeb50aad9a658fc
Mario Torres <scaramuzzini@gmail.com>
2025-07-04 11:10:58 -0400

    Logica do vencedor implementada

--- src/Jogo.jsx
+++ src/Jogo.jsx
@@ -3,8 +3,38 @@ import { useState } from 'react';
 
 function Quadrado({quad,handleClick}) {
     return <button className='quadrado' 
-            onClick={handleClick}>
-        {quad}</button>
+            onClick={handleClick}>{quad}</button>
+}
+
+function verificarVencedor(q) {
+    /* AND (E)
+        V e V = V
+        V e F = F
+        F e V = F
+        F e F = F
+        */
+
+    const combinacoesVencedoras = [    
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+
+        [0, 4, 8],
+        [6, 4, 2]
+    ];
+
+    for (let i=0; i < combinacoesVencedoras.length; i++) {
+        const [pa, pb, pc] = combinacoesVencedoras[i];
+        if (q[pa] && q[pa] == q[pb] && q[pb] == q[pc]) {
+            return q[pa];
+        }
+    }
+
+    return null;
 }
 
 function Tabuleiro() {
@@ -12,22 +42,33 @@ function Tabuleiro() {
     const [quadrados, setQuadrados] = useState(Array(9).fill(null));
     
     function handleClickQuad(i) {
-        if (quadrados[i] == null) {
-            const novosQuadrados = quadrados.slice();
-            novosQuadrados[i] = vezDoX ? 'X' : 'O';
-            setQuadrados(novosQuadrados);
-            setVezDoX(!vezDoX);
-        } else {
-            alert('movimento invalido!');
+        if (verificarVencedor(quadrados) || quadrados[i]) {
+            return;
         }
+        const novosQuadrados = quadrados.slice();
+        novosQuadrados[i] = vezDoX ? 'X' : 'O';
+        setQuadrados(novosQuadrados);
+        setVezDoX(!vezDoX);
     }
 
     function reiniciarJogo() {
         setQuadrados(Array(9).fill(null));
+        setVezDoX(true);
+    }
+
+    
+
+    let vencedor = verificarVencedor(quadrados);
+    let mensagem;
+    if (vencedor) {
+        mensagem = 'O vencedor é '+vencedor;
+    } else {
+        mensagem = 'Vez do Jogador:' + (vezDoX ? 'X' : 'O')
     }
 
     return (
         <>
+            <h2>{mensagem}</h2>
             <button onClick={reiniciarJogo}>Reiniciar o jogo</button>
             <div className='linha'>
                 <Quadrado quad={quadrados[0]} handleClick={() => handleClickQuad(0)} />
