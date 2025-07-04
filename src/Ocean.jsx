49f94443279222b65465e7f5afea77823985e652
Mario Torres <scaramuzzini@gmail.com>
2025-07-04 09:37:11 -0400

    Botao aleatorio

--- src/Ocean.jsx
+++ src/Ocean.jsx
@@ -1,8 +1,22 @@
-export default function MeuApp() {
+function MeuApp() {
     return (
         <>
             <h1>Ocean Brasil!!!</h1>
             <p>Esta é uma aula de reactjs</p>
+            <BotaoAleatorio />
+            <BotaoAleatorio />
+            <BotaoAleatorio />
+            <BotaoAleatorio />
         </>
     )
 }
+
+function BotaoAleatorio() {
+    return (
+        <button onClick={() => console.log('Botão clicado!!!')}>
+            Clique
+        </button>
+    )
+}
+
+export default MeuApp;
