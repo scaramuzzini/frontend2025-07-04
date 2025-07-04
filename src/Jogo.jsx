695827c0cd781502585fac04b0476560c5b4f81c
Mario Torres <scaramuzzini@gmail.com>
2025-07-04 09:58:43 -0400

    Jogo e tabuleiro criados

--- src/Jogo.jsx
+++ src/Jogo.jsx
@@ -1,7 +1,33 @@
+import './jogo.css'
+
+function Tabuleiro() {
+    return (
+        <>
+            <div className='linha'>
+                <button className='quadrado'>1</button>
+                <button className='quadrado'>2</button>
+                <button className='quadrado'>3</button>
+            </div>
+            <div className='linha'>
+                <button className='quadrado'>1</button>
+                <button className='quadrado'>2</button>
+                <button className='quadrado'>3</button>
+            </div>
+            <div className='linha'>
+                <button className='quadrado'>1</button>
+                <button className='quadrado'>2</button>
+                <button className='quadrado'>3</button>
+            </div>
+        </>
+    )
+
+}
+
 function JogoDaVelha() {
     return (
         <>
             <h1>Jogo da Velha</h1>
+            <Tabuleiro />
         </> 
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import MeuApp from './Ocean.jsx'
+import JogoDaVelha from './Jogo.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MeuApp />
+    <JogoDaVelha />
   </StrictMode>,
 )
