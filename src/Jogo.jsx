f02281bb9f4711d580673996293dab81c6cda045
Mario Torres <scaramuzzini@gmail.com>
2025-07-04 09:49:20 -0400

    contador

---
+++ src/Jogo.jsx
@@ -0,0 +1,9 @@
+function JogoDaVelha() {
+    return (
+        <>
+            <h1>Jogo da Velha</h1>
+        </> 
+    )
+}
+
+export default JogoDaVelha
--- src/Ocean.jsx
+++ src/Ocean.jsx
@@ -1,12 +1,16 @@
+import { useState } from 'react';
+
 function MeuApp() {
+    const [contador,setContador] = useState(0);
+
     return (
         <>
             <h1>Ocean Brasil!!!</h1>
             <p>Esta é uma aula de reactjs</p>
-            <BotaoAleatorio />
-            <BotaoAleatorio />
-            <BotaoAleatorio />
-            <BotaoAleatorio />
+            <p>Você clicou {contador} vezes</p>
+            <button onClick={() => setContador(contador + 10)}>
+                Somar 10
+            </button>
         </>
     )
 }
